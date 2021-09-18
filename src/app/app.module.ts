ca855f8afc1198882d8a80ba4cd9fa82303d80bc
Dpmqueiroz <danielqueiroz94@gmail.com>
2021-09-18 12:01:29 -0300

    pipe com data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TopMenuComponent } from './components/top-menu/top-menu.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -17,6 +16,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { RankingComponent } from './components/ranking/ranking.component';
 import { NovaCompeticaoComponent } from './components/nova-competicao/nova-competicao.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -38,9 +42,15 @@ import { FormsModule } from '@angular/forms';
     MatIconModule,
     HttpClientModule,
     MatGridListModule,
-    FormsModule
+    FormsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatMenuModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
