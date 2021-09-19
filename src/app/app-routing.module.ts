ff46dacf33221b6c0fa90192c221fe39e7aca92b
Dpmqueiroz <danielqueiroz94@gmail.com>
2021-09-19 10:50:13 -0300

    Rotas Funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NovaCompeticaoComponent } from './components/nova-competicao/nova-competicao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "olimpiadas", pathMatch: 'full'},
+  { path: "olimpiadas", component: NavbarComponent },
+  { path: "edit", component: NovaCompeticaoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
